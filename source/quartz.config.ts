f79b08da59a23deac82e7d67b3277c45853a60e9
Pranjal Rai <pranjalraiiitd@gmail.com>
2025-06-26 15:34:05 +0530

    essays

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "defectum",
+    pageTitle: "Defectum",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
