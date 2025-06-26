44a2242e7a334bd5838b4606a91543af7338d406
Pranjal Rai <pranjalraiiitd@gmail.com>
2025-06-26 15:22:16 +0530

    essays

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Lavishly Yours",
+        header: "JetBrains Mono",
         body: "JetBrains Mono",
         code: "JetBrains Mono"
       },
